e258cd2e79ec8fbba4628106b7a60c2b2f4615a1
rahulrm33 <71714828+rahulrm33@users.noreply.github.com>
2021-02-05 00:02:17 +0530

    changed for features to get humidity,

--- src/utility/forecast.js
+++ src/utility/forecast.js
@@ -11,7 +11,7 @@ const forecast=(latitude,longitude,callback)=>{
         callback('Unable to find the weather.Try another location',undefined)
     }
     else{
-        callback(undefined,body.current.weather_descriptions[0]+' .It is currently '+body.current.temperature+' degrees out.It feels like '+body.current.precip+' degrees out')
+        callback(undefined,body.current.weather_descriptions[0]+' .It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.'+' And humidity is around '+body.current.humidity+'%.')
     }
     })
 }
